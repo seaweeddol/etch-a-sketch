8d01104fe4ba85f955aaa4ab1f989bd49cb2c2d3
Jessica <42528319+seaweeddol@users.noreply.github.com>
2019-02-22 23:58:39 -0800

    Add random color mode

--- script/script.js
+++ script/script.js
@@ -1,17 +1,17 @@
 const grid = document.querySelector('.grid');
 const resetButton = document.querySelector('#reset')
+const colorModeButton = document.querySelector('#color')
 
 createGrid(16);
-
-// fill in cell when hovered
-grid.addEventListener('mouseover', (e) => {
-  if (e.target.classList.contains('grid') == false){
-    e.target.classList.add("fill");
-  }
-})
+blackFill();
 
 resetButton.addEventListener('click', (e) => {
   reset();
+})
+
+colorModeButton.addEventListener('click', (e) => {
+  reset();
+  randomFill();
 })
 
 // create square grid based on number provided
@@ -33,4 +33,32 @@ function reset(){
     userGrid = prompt("Select a number between 1-100 to create a new grid.");
   }
   createGrid(userGrid);
+}
+
+// fill in cell when hovered
+function blackFill(){
+  grid.addEventListener('mouseover', (e) => {
+    if (e.target.classList.contains('grid') == false){
+      e.target.classList.add("fill");
+    }
+  })
+}
+
+// generate a random rgb color
+function randomColor(){
+  let r = Math.floor(Math.random() * 255);
+  let b = Math.floor(Math.random() * 255);
+  let g = Math.floor(Math.random() * 255);
+
+  let rgb = 'rgb(' + r + ', ' + b + ', ' + g + ')';
+  return rgb;
+}
+
+// add event listener for random color fill
+function randomFill(){
+  grid.addEventListener('mouseover', (e) => {
+    if (e.target.classList.contains('grid') == false){
+      e.target.style.background = randomColor();
+    }
+  })
 }
