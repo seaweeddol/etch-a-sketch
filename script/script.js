07ee215b1ad3c87686f80e91036368d62bf15136
Jessica <42528319+seaweeddol@users.noreply.github.com>
2019-02-22 23:43:06 -0800

    Update class name to grid from container

--- script/script.js
+++ script/script.js
@@ -1,11 +1,11 @@
-const grid = document.querySelector('.container');
+const grid = document.querySelector('.grid');
 const resetButton = document.querySelector('#reset')
 
 createGrid(16);
 
 // fill in cell when hovered
 grid.addEventListener('mouseover', (e) => {
-  if (e.target.classList.contains('container') == false){
+  if (e.target.classList.contains('grid') == false){
     e.target.classList.add("fill");
   }
 })
@@ -17,7 +17,7 @@ resetButton.addEventListener('click', (e) => {
 // create square grid based on number provided
 function createGrid(rowsCols){
   for (let i = 0; i < rowsCols*rowsCols; i++) {
-    const grid = document.querySelector('.container');
+    const grid = document.querySelector('.grid');
     grid.setAttribute('style', 'grid-template-columns: repeat(auto-fit, minmax(' + 100/rowsCols + '%, 1fr))');
     const cell = document.createElement('div');
     cell.classList.add('cell');
@@ -30,7 +30,7 @@ function reset(){
   grid.innerHTML = '';
   let userGrid = 0;
   while(userGrid <= 0 || userGrid >= 100) {
-    userGrid = prompt("Select a number between 1-100 for the grid size.");
+    userGrid = prompt("Select a number between 1-100 to create a new grid.");
   }
   createGrid(userGrid);
 }
