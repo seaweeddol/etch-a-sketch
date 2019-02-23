00ead2d1a9a8a2c2b3a99c6d46afea28e8fe48c5
Jessica <42528319+seaweeddol@users.noreply.github.com>
2019-02-22 21:52:50 -0800

    Add function to create grid

--- script/script.js
+++ script/script.js
@@ -1,1 +1,9 @@
-console.log("hi");
+createGrid(16);
+
+function createGrid(rowsCols){
+  for (let i = 0; i < rowsCols*rowsCols; i++) {
+    const grid = document.querySelector('.container');
+    const cell = document.createElement('div');
+    grid.appendChild(cell);
+  }
+}
