c7c7acfdf084452b51858b6800872274046a7345
Jessica <42528319+seaweeddol@users.noreply.github.com>
2019-02-22 22:40:41 -0800

    Add event listener to fill in cell when hovered over

--- script/script.js
+++ script/script.js
@@ -1,9 +1,19 @@
+const grid = document.querySelector('.container');
+
 createGrid(16);
 
 function createGrid(rowsCols){
   for (let i = 0; i < rowsCols*rowsCols; i++) {
     const grid = document.querySelector('.container');
     const cell = document.createElement('div');
+    cell.classList.add('cell');
     grid.appendChild(cell);
   }
 }
+
+// fill in cell when hovered
+grid.addEventListener('mouseover', (e) => {
+  if (e.target.classList.contains('container') == false){
+    e.target.classList.add("fill");
+  }
+})
